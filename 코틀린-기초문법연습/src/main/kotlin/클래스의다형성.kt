de89b7f251929247971285055d8a9d637ba31eb0
its-sky <smc9919@naver.com>
2023-07-04 13:59:00 +0900

    코틀린-기초문법연습 Updated at. 20230704.1358

--- 코틀린-기초문법연습/src/main/kotlin/클래스의다형성.kt
+++ 코틀린-기초문법연습/src/main/kotlin/클래스의다형성.kt
@@ -1,4 +1,34 @@
 fun main() {
+    var a = Drink()
+    a.drink()
 
+    var b: Drink = Cola()
+    b.drink()
+    if (b is Cola) {
+        b.washDishes()
+    } // is로 타입체크
+
+    var c = b as Cola // as로 다운캐스팅 가능
+    c.washDishes()
+    b.washDishes()
 }
 
+open class Drink {
+    var name = "음료"
+
+    open fun drink() {
+        println("${name}을 마십니다")
+    }
+}
+
+class Cola : Drink() {
+    var type = "콜라"
+
+    override fun drink() {
+        println("${name}중에 ${type}을 마십니다")
+    }
+
+    fun washDishes() {
+        println("${type}로 설거지를 합니다")
+    }
+}
